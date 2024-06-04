b18bd0b808525d2fba0cbf4a19b5dac5ce45de96
Hephzisoft <hephzisoft@gmail.com>
2024-06-04 21:34:57 +0100

    Finished the Splash screen

--- lib/features/splash/view/widget/splash_widget.dart
+++ lib/features/splash/view/widget/splash_widget.dart
@@ -1,9 +1,11 @@
+import 'package:app_book/common/widgets/button_widget.dart';
 import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
+import '../../../../common/routes/route_constant.dart';
 import '../../../../common/utils/image_constant.dart';
 import '../../controller/splash_controller.dart';
 
@@ -51,20 +53,25 @@ class PageViewWidget extends ConsumerWidget {
           height: 20.h,
         ),
         Container(
+          alignment: Alignment.center,
           width: 300.w,
           padding: EdgeInsets.symmetric(horizontal: 15.w),
           child: Text(
             subTitle,
-            softWrap: true,
-            style: const TextStyle(),
+            // softWrap: true,
+            textAlign: TextAlign.center,
           ),
         ),
         SizedBox(
           height: 30.h,
         ),
-        DotsIndicatorWidget(
-          dotsCount: 3,
-          position: index,
+        Container(
+          alignment: Alignment.center,
+          height: 100.h,
+          child: DotsIndicatorWidget(
+            dotsCount: 3,
+            position: index,
+          ),
         )
       ],
     );
@@ -107,7 +114,83 @@ class ThirdPageViewWidget extends StatelessWidget {
       children: [
         SvgPicture.asset(ImageConstant.onboardingImage3),
         Positioned(
+          top: 100,
+          right: 110,
           child: Image.asset(ImageConstant.effectivePeopleBook),
+        ),
+        Positioned(
+          right: 120,
+          top: 280,
+          child: Image.asset(ImageConstant.ikigaiBook),
+        ),
+        Positioned(
+          right: 120,
+          top: 430,
+          child: Image.asset(ImageConstant.startUpBook),
+        ),
+        Positioned(
+          left: -10,
+          top: 100,
+          child: Image.asset(ImageConstant.psycologyBook),
+        ),
+        Positioned(
+          bottom: 300,
+          child: Image.asset(ImageConstant.stealLikeBook),
+        ),
+        Positioned(
+          top: 80,
+          right: 0,
+          child: Image.asset(ImageConstant.theAlchemistBook),
+        ),
+        Positioned(
+          bottom: 300,
+          right: 0,
+          child: Image.asset(ImageConstant.richDadBook),
+        ),
+        Positioned(
+          bottom: 50,
+          child: Container(
+            width: 380.w,
+            padding: EdgeInsets.symmetric(horizontal: 15.w),
+            child: Column(
+              children: [
+                Text(
+                  'Book Has Power To Change Everything',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20.sp,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 10.h,
+                ),
+                SizedBox(
+                  width: 300.w,
+                  child: const Text(
+                    'We have true friend in our life and the books is that. Book has power to chnage yourself and make you more valueable.',
+                    // softWrap: true,
+                    textAlign: TextAlign.center,
+                    // style: TextStyle(),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(
+                    top: 10.h,
+                  ),
+                  width: 280.w,
+                  child: ButtonWidget(
+                    text: 'Get Started Now',
+                    verticalPadding: 14,
+                    onPressed: () {
+                      Navigator.of(context)
+                          .pushReplacementNamed(RouteConstant.login);
+                    },
+                  ),
+                )
+              ],
+            ),
+          ),
         )
       ],
     );
